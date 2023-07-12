c9676803d46927a49aa80f4b9a7cd9d6bbee4a2f
Md Uzzal Hossain <106810652+UzzalVau@users.noreply.github.com>
2023-07-12 20:16:37 +0600

    Add files via upload

--- R3D.py
+++ R3D.py
@@ -143,12 +143,10 @@ def banner():
 	sol().print(cik2)
 	ban='''
 •   AUTHOR  : AOREC-XD WHATSAPP : 01843438888  •
-•   _____ _______  ____________________         •
-•   |  |  |  | |       /  |       /     /   \     |  |     
-    |  |  |  | `---/  /   `---/  /     /  ^  \    |  |     
-    |  |  |  |    /  /       /  /     /  /_\  \   |  |     
-    |  `--'  |   /  /----.  /  /----./  _____  \  |  `----.
-     \______/   /________| /________/__/     \__\ |_______|VERSION 4.0.4  •
+•        __ __           
+|  |  /  /  /\  |    
+\__/ /_ /_ /~~\ |___ 
+                     VERSION 4.0.4  •
 •                                               •
 •   GITHUB : https://github.com/UzzalVau   •'''
 	oi = nel(tekz(ban,justify='center',style='bold'), style='cyan')
